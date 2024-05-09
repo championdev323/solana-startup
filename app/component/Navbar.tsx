e66bdf4613811e943889250e68bec8140065f30b
championdev323 <championbizzybee@gmail.com>
2024-05-09 13:32:08 -0700

    feat: update the main page style

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import React from 'react';
-import dynamic from 'next/dynamic';
-import { useWallet } from '@solana/wallet-adapter-react';
+import React from "react";
+import dynamic from "next/dynamic";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 const WalletMultiButtonDynamic = dynamic(
   async () =>
-    (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
   { ssr: false }
 );
 
@@ -14,15 +14,15 @@ const NavBar = () => {
   const wallet = useWallet();
 
   return (
-    <div className='flex justify-between p-4 border bg-gray-300'>
-      <div className='flex items-center space-x-4'>
-        <h3 className='font-serif'>Aychdev</h3>
+    <div className="flex justify-between p-4 border bg-gray-300">
+      <div className="flex items-center space-x-4">
+        <h3 className="font-serif">Aychdev</h3>
       </div>
       <div>
         <WalletMultiButtonDynamic>
           {wallet.publicKey
             ? `${wallet.publicKey.toBase58().substring(0, 7)}...`
-            : 'Connect Wallet'}
+            : "Connect Wallet"}
         </WalletMultiButtonDynamic>
       </div>
     </div>
